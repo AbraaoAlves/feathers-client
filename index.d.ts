1201ecec8e8019edd0268d20ee1a0e3416dcea37
abraaoalves <abraao.teodosio@gmail.com>
2017-01-09 10:54:17 -0300

    chore(definition): use definitions on dependencies

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,4 @@
+<<<<<<< 4044c968f764c8dd1942ae36b99edab42324dde9
 import * as core from 'feathers/client';
 import * as restDep from 'feathers-rest/client';
 import * as socketioDep from 'feathers-socketio/client';
@@ -28,3 +29,22 @@ declare namespace e{
 }
 
 export = e;
+=======
+import feathers from 'feathers/client';
+import restDep from 'feathers-rest/client';
+import socketioDep from 'feathers-socketio/client';
+import primusDep from 'feathers-primus/client';
+import hooksDep from 'feathers-hooks';
+import authenticationDep from 'feathers-authentication-client';
+
+export default e;
+
+declare function e(): feathers.Application;
+declare namespace e{
+  const rest: typeof restDep;
+  const socketio: typeof socketioDep;
+  const primus: typeof primusDep;
+  const hooks: typeof hooksDep;
+  const authentication: typeof authenticationDep;
+}
+>>>>>>> chore(definition): use definitions on dependencies
