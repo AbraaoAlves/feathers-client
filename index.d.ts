ef6fb48fd2656a3624014204d5cb6f13a8fdc355
abraaoalves <abraao.teodosio@gmail.com>
2017-01-14 23:14:59 -0300

    fix(definition): rename default import remove unused default export

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
-import feathers from 'feathers/client';
+import core from 'feathers/client';
 import restDep from 'feathers-rest/client';
 import socketioDep from 'feathers-socketio/client';
 import primusDep from 'feathers-primus/client';
@@ -6,22 +6,19 @@ import hooksDep from 'feathers-hooks';
 import authenticationDep from 'feathers-authentication-client';
 
 export as namespace feathers;
-
-export default e;
- 
 declare function e(): e.Application;
 
 declare namespace e{
-  const rest: typeof restDep;
-  const socketio: typeof socketioDep;
-  const primus: typeof primusDep;
-  const hooks: typeof hooksDep;
-  const authentication: typeof authenticationDep;
+  export const rest: typeof restDep;
+  export const socketio: typeof socketioDep;
+  export const primus: typeof primusDep;
+  export const hooks: typeof hooksDep;
+  export const authentication: typeof authenticationDep;
 
-  interface ServiceParams         extends feathers.Params{}
-  interface FeathersUseHandler<T> extends feathers.FeathersUseHandler<T>{}
-  interface Service<T>            extends feathers.Service<T>{}
-  interface Application           extends feathers.Application{
+  interface ServiceParams         extends core.Params{}
+  interface FeathersUseHandler<T> extends core.FeathersUseHandler<T>{}
+  interface Service<T>            extends core.Service<T>{}
+  interface Application           extends core.Application{
     passport: authenticationDep.Passport;
     authenticate: (credentials?: authenticationDep.Credentials) => Promise<any>;
     logout: () => Promise<any>;
@@ -29,3 +26,5 @@ declare namespace e{
 
   interface AuthConfig extends authenticationDep.Config {} 
 }
+
+export = e;
